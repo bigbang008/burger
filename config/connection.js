828083e4d61c4e969491467452845944adeca13c
SarawutM <imagination.mystery@gmail.com>
2018-06-28 22:19:41 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,13 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "250833sS",
-  database: "burgers_db"
-});
+    var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "250833sS",
+      database: "burgers_db"
+    });
 };
 
 // Make connection.
