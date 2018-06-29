544ed04ce9c0ba09da575fd04cfa2ec11c341bc4
SarawutM <imagination.mystery@gmail.com>
2018-06-28 21:29:58 -0500

    "add files"

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Double Chesse', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Angus Beef', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Quarter Pounder', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Crispy Chicken', false, CURRENT_TIMESTAMP);
